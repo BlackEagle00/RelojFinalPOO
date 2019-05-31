2c06b823967004e32154fee368fd7eb56913626c
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2019-05-31 08:33:46 -0500

    Update PanelFormato.java

--- ProyectoReloj-POO/src/interfaz/PanelFormato.java
+++ ProyectoReloj-POO/src/interfaz/PanelFormato.java
@@ -1,14 +1,38 @@
 package interfaz;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 import controlador.Controlador;
 
-public class PanelFormato extends JPanel {
+public class PanelFormato extends JPanel 
+{
 	
-private Controlador ctrl;
+	private JRadioButton btn12;
+	private JRadioButton btn24;
+	private ButtonGroup btn_format;
 	
-	public PanelFormato(Controlador ctrl) {
+	private Controlador ctrl;
+	
+	public PanelFormato(Controlador ctrl)
+	{
+		
+		setSize(350,100);
+		setLayout(null);
+		setBorder(new CompoundBorder(new EmptyBorder(0, 0, 0, 0), new TitledBorder("Formato")));
+		
+		btn12 = new JRadioButton(" 12 Horas ", true);
+		add(btn12);
+	    btn24 = new JRadioButton(" 24 Horas ", false);
+	    btn_format = new ButtonGroup();
+	    add(btn24);
+
+		
 		this.ctrl = ctrl;
 	}
 
