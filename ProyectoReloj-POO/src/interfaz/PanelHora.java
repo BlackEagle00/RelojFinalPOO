a65f45fe24c4741d825e8ffbea3a51bc95452978
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2019-05-31 08:34:20 -0500

    Update PanelHora.java

--- ProyectoReloj-POO/src/interfaz/PanelHora.java
+++ ProyectoReloj-POO/src/interfaz/PanelHora.java
@@ -6,16 +6,23 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
-public class PanelHora extends JPanel {
+public class PanelHora extends JPanel 
+{
 	
 	JLabel hora;
 	JLabel minutos;
 	JLabel segundos;
+	
+	JLabel h;
+	JLabel m;
+	JLabel s;
+	
 	JLabel sHorario;
 	
-	public PanelHora() {
+	public PanelHora() 
+	{
 	
-		setSize(300,115);
+		setSize(350,100);
 		setLayout(null);
 		setBorder(new CompoundBorder(new EmptyBorder(0, 0, 0, 0), new TitledBorder("Hora")));
 		
@@ -23,14 +30,29 @@ public class PanelHora extends JPanel {
 		hora.setBounds(25, 25, 30, 15);
 		add(hora);
 		
+		h = new JLabel("12");
+		h.setBounds(25, 50, 30, 15);
+		add(h);
+		
 		minutos = new JLabel("Minutos");
-		minutos.setBounds(100, 25, 45, 15);
+		minutos.setBounds(110, 25, 45, 15);
 		add(minutos);
+		
+		m = new JLabel("0");
+		m.setBounds(110, 50, 45, 15);
+		add(m);
 		
 		segundos = new JLabel("Segundos");
 		segundos.setBounds(200, 25, 60, 15);
 		add(segundos);
+	
+		s = new JLabel("0");
+		s.setBounds(200, 50, 60, 15);
+		add(s);
 		
+		sHorario = new JLabel("AM");
+		sHorario.setBounds(310, 50, 60, 15);
+		add(sHorario);
 	}
 
 }
