9b3fced8d26feb81ef6b719c596813fc9d0de179
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2019-05-31 08:33:05 -0500

    Update InterfazApp.java

--- ProyectoReloj-POO/src/interfaz/InterfazApp.java
+++ ProyectoReloj-POO/src/interfaz/InterfazApp.java
@@ -4,32 +4,44 @@ import javax.swing.JFrame;
 
 import controlador.Controlador;
 
-public class InterfazApp extends JFrame {
+public class InterfazApp extends JFrame 
+{
 
 	private Controlador ctrl;
 	private PanelHora pnlHora;
 	private PanelAjustes pnlAjustes;
 	private PanelFormato pnlFormato;
 	
-	public InterfazApp(Controlador ctrl) {
+	public InterfazApp(Controlador ctrl) 
+	{
 		
 		this.ctrl = ctrl;
+		
 		pnlHora = new PanelHora();
-		pnlAjustes = new PanelAjustes(ctrl);
-		pnlFormato = new PanelFormato(ctrl);
+	    pnlHora.setBounds(5, 5, 340, 80);		
+
+	    pnlAjustes = new PanelAjustes(ctrl);
+	    pnlAjustes.setBounds(5, 95, 340, 80);   
+
+	    pnlFormato = new PanelFormato(ctrl);
+	    pnlFormato.setBounds(5, 185, 340, 80);
+	     
 		ctrl.conectar(pnlHora);
 		
-		setSize(310,360);
+		setSize(360,310);
 		setLayout(null);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		
 		add(pnlHora);
+		add(pnlAjustes);
+		add(pnlFormato);
 		
 		setVisible(true);
 		
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		InterfazApp interfaz = new InterfazApp(new Controlador());
 	}
 }
